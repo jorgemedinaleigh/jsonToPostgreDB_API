96b43eedcfb405c47a3e4b39525fda146094d727
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-01-31 15:18:14 -0300

    Innitial Commit

---
+++ orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Controllers/CuentaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace orion_msdynamics_inbound_api.Controllers
+{
+    public class CuentaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Models/Cuenta.cs
@@ -0,0 +1,76 @@
+﻿
+namespace orion_msdynamics_inbound_api.Models
+{
+    public class Cuenta
+    {
+        public string telephone1 { get; set; }
+        public string websiteurl { get; set; }
+        public string ownerid { get; set; }
+        public string primarycontactid { get; set; }
+        public string name { get; set; }
+        public string fax { get; set; }
+        public string parentaccountid { get; set; }
+        public string industrycode { get; set; }
+        public string sic { get; set; }
+        public string ownershipcode { get; set; }
+        public string description { get; set; }
+        public string originatingleadid { get; set; }
+        public bool donotsendmm { get; set; }
+        public string preferredcontactmethodcode { get; set; }
+        public bool donotemail { get; set; }
+        public bool followemail { get; set; }
+        public bool donotbulkemail { get; set; }
+        public bool donotphone { get; set; }
+        public bool donotfax { get; set; }
+        public bool donotpostalmail { get; set; }
+        public string transactioncurrencyid { get; set; }
+        public double creditlimit { get; set; }
+        public bool creditonhold { get; set; }
+        public string paymenttermscode { get; set; }
+        public string address1_shippingmethodcode { get; set; }
+        public string address1_freighttermscode { get; set; }
+        public string tickersymbol { get; set; }
+        public string accountnumber { get; set; }
+        public string emailaddress1 { get; set; }
+        public string telephone2 { get; set; }
+        public string address1_addresstypecode { get; set; }
+        public string address1_name { get; set; }
+        public string address1_line1 { get; set; }
+        public string address1_line2 { get; set; }
+        public string address1_line3 { get; set; }
+        public string address1_city { get; set; }
+        public string address1_stateorprovince { get; set; }
+        public string address1_postalcode { get; set; }
+        public string address1_country { get; set; }
+        public string address1_telephone1 { get; set; }
+        public decimal mbo_ingresosanuales { get; set; }
+        public int mbo_numerodeempleados { get; set; }
+        public string accountcategorycode { get; set; }
+        public string customertypecode { get; set; }
+        public string defaultpricelevelid { get; set; }
+        public string preferredappointmentdaycode { get; set; }
+        public string preferredsystemuserid { get; set; }
+        public string preferredappointmenttimecode { get; set; }
+        public string mbo_razonsocial { get; set; }
+        public bool mbo_cuentafoco { get; set; }
+        public string mbo_perfilfacebook { get; set; }
+        public string mbo_perfillinkedin { get; set; }
+        public string mbo_perfiltwitter { get; set; }
+        public string mbo_origendelcandidato { get; set; }
+        public string address1_composite { get; set; }
+        public string mbo_pais { get; set; }
+        public string mbo_region { get; set; }
+        public string mbo_provincia { get; set; }
+        public string mbo_comuna { get; set; }
+        public string mbo_zonadeventascuenta { get; set; }
+        public string mbo_clasificaciondelaempresaenelconglomerado { get; set; }
+        public string mbo_clasificacionempresa { get; set; }
+        public string mbo_origendelaempresa { get; set; }
+        public string mbo_sn { get; set; }
+        public string emailaddress2 { get; set; }
+        public string emailaddress3 { get; set; }
+        public string mbo_correofacturacionelectronica { get; set; }
+        public string mbo_codigosn { get; set; }
+        public string mbo_origensap { get; set; }
+    }
+}
