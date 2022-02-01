a27008ec2df5ae7c31c871d84684a4700d1ed84a
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-02-01 18:46:25 -0300

    Added connection to DB & method to insert

--- orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Controllers/CuentaController.cs
+++ orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Controllers/CuentaController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using orion_msdynamics_inbound_api.Data;
+using orion_msdynamics_inbound_api.Models;
 
 namespace orion_msdynamics_inbound_api.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class CuentaController : Controller
     {
-        public IActionResult Index()
+        private readonly CuentaRepository _cuentaRepository = new CuentaRepository();
+
+        [HttpPost]
+        public void CuentaLogger(Cuenta cuenta)
         {
-            return View();
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string cuentaJson = JsonSerializer.Serialize(cuenta, options);
+            _cuentaRepository.AddCuenta(cuentaJson);
         }
     }
 }
---
+++ orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Data/CuentaRepository.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+using orion_msdynamics_inbound_api.Models;
+
+namespace orion_msdynamics_inbound_api.Data
+{
+    public class CuentaRepository
+    {
+        private const string CONNECTION_STRING = "Host=localhost:5432;" +
+                                                 "Username=postgres;" +
+                                                 "Password=sfr-66t.1337;" +
+                                                 "Database=postgres";
+
+        private NpgsqlConnection connection;
+
+        public CuentaRepository()
+        {
+            connection = new NpgsqlConnection(CONNECTION_STRING);
+            connection.Open();
+        }
+
+        public void AddCuenta(string cuenta)
+        {
+            string commandText = @"insert into public.""AccountLogs""(""JsonFile"") values ('@file');";
+            using (var cmd = new NpgsqlCommand(commandText, connection))
+            {
+                cmd.Parameters.AddWithValue("file", cuenta);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
