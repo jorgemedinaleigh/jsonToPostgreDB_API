c95c1e5312af13a879f9b11ad26f204047504d7d
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-02-02 17:04:34 -0300

    Added response

--- orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Controllers/CuentaController.cs
+++ orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Controllers/CuentaController.cs
@@ -11,12 +11,28 @@ namespace orion_msdynamics_inbound_api.Controllers
     public class CuentaController : Controller
     {
         private readonly CuentaRepository _cuentaRepository = new CuentaRepository();
+        ResponseData _responseData = new ResponseData();
 
         [HttpPost]
-        public void CuentaLogger(Cuenta cuenta)
+        public ResponseData CuentaLogger(Cuenta cuenta)
         {
-            string cuentaJson = JsonSerializer.Serialize(cuenta);
-            _cuentaRepository.AddCuenta(cuentaJson);
+            try
+            {
+                string cuentaJson = JsonSerializer.Serialize(cuenta);
+                _cuentaRepository.AddCuenta(cuentaJson);
+
+                _responseData.status = "Success";
+                _responseData.message = "Cuenta registrada en log de base de datos";
+                _responseData.statusCode = 0;
+            }
+            catch (Exception ex)
+            {
+                _responseData.status = "Failed";
+                _responseData.message = "Ocurrio un error al registrar la informacion";
+                _responseData.statusCode = ex.GetHashCode();
+            }
+
+            return _responseData;
         }
     }
 }
---
+++ orion_msdynamics_inbound_api/orion_msdynamics_inbound_api/Models/ResponseData.cs
@@ -0,0 +1,9 @@
+﻿namespace orion_msdynamics_inbound_api.Models
+{
+    public class ResponseData
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public int statusCode { get; set; }
+    }
+}
